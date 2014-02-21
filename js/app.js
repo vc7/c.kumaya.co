73681493d558977f4ba37f3c51065d453077f235
vincent <vince78718@gmail.com>
2014-02-21 13:54:52 +0800

    Update comics

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function ComicController($scope) {
 	var comics = [];
-	for(var i=428; i>=416; i--)
+	for(var i=436; i>=416; i--)
 	{
 		comics.push(i);
 	}
 	console.log(comics);
 
 	$scope.comics = comics;
-	$scope.nextEpisode = 429;
+	$scope.nextEpisode = 437;
 }
