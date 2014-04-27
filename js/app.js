2029e0c30cb90387206eaeba29a9470ee8a70a73
Vincent Chen <vince78718@gmail.com>
2014-04-27 22:11:11 +0800

    Adding episode 444 to 452.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function ComicController($scope) {
 	var comics = [];
-	for(var i=443; i>=416; i--)
+	for(var i=452; i>=416; i--)
 	{
 		comics.push(i);
 	}
 	console.log(comics);
 
 	$scope.comics = comics;
-	$scope.nextEpisode = 444;
+	$scope.nextEpisode = 453;
 }
