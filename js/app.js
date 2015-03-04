d9024e49d441316c7c264a1d7adf4712fa2a8240
Vincent Chen <vince78718@gmail.com>
2015-03-05 00:58:03 +0800

    Update comics from 525 to 532.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function ComicController($scope) {
 	var comics = [];
-	for(var i=524; i>=416; i--)
+	for(var i=532; i>=416; i--)
 	{
 		comics.push(i);
 	}
 	console.log(comics);
 
 	$scope.comics = comics;
-	$scope.nextEpisode = 525;
+	$scope.nextEpisode = 534;
 }
