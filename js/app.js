e1b4e6db413c169bfe67b2e2617e5b56acf70fdb
Vincent Chen <vince78718@gmail.com>
2015-06-20 01:14:02 +0800

    Add comics 549 to 556.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function ComicController($scope) {
 	var comics = [];
-	for(var i=548; i>=416; i--)
+	for(var i=556; i>=416; i--)
 	{
 		comics.push(i);
 	}
 	console.log(comics);
 
 	$scope.comics = comics;
-	$scope.nextEpisode = 549;
+	$scope.nextEpisode = 557;
 }
